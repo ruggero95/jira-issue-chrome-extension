8aad64f0bc7f08c45852fac13f7e532b1e6f7d50
ruggero95 <ruggero.panzeri95@gmail.com>
2023-07-18 19:37:24 +0200

    jira board response

---
+++ src/api/jira.board.ts
@@ -0,0 +1,15 @@
+export type Board =  {
+    "id": number;//1,
+    "self": string;//"https://team-1602237711474.atlassian.net/rest/agile/1.0/board/1",
+    "name": string; //"BIT board",
+    "type": string; //"simple",
+    "location": {
+        "projectId": number;//10000,
+        "displayName": string; //"CherryBit (BIT)",
+        "projectName": string; //"CherryBit",
+        "projectKey": string; //"BIT",
+        "projectTypeKey": string; //"software",
+        "avatarURI": string; //"https://team-1602237711474.atlassian.net/rest/api/2/universal_avatar/view/type/project/avatar/10551?size=small",
+        "name": string; //"CherryBit (BIT)"
+    }
+}
--- src/api/jira.respons.ts
+++ src/api/jira.respons.ts
@@ -1,12 +1,15 @@
+import { Board } from "./jira.board";
 import { Issue } from "./jira.issue";
 
 export type JiraResponse<T> = {
-    "expand": string;//"schema,names",
+    "expand"?: string;//"schema,names",
     "startAt": number;// 0,
     "maxResults": number;//150,
     "total": number;//48,
+    "isLast"?:boolean;
 } & T
 
 
 
 export type JiraIssueResponse = JiraResponse<{issues: Issue[]}>
+export type JiraBoardResponse = JiraResponse<{values: Board[]}>
