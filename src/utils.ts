6140b92172052c3986c2107e89f3c710b1c027d7
ruggero95 <ruggero.panzeri95@gmail.com>
2023-07-20 23:45:35 +0200

    fix storage

--- src/utils.ts
+++ src/utils.ts
@@ -1,48 +1,18 @@
 import axios from "axios"
-import { SettingsI } from "./components/pages/Settings"
+import { SettingsI } from "./components/context/settingsContext"
 import { StorageEnum } from "./storage"
 
 export const setChromeStorage = async (key: string, value: string) => {
-    console.log(process.env.NODE_ENV)
-    if (process.env.NODE_ENV === "development") {
-        console.log('classic storage')
+    return window.localStorage.setItem(key, value)
+}
 
-        return window.localStorage.setItem(key, value)
-    }
-    console.log('storage extension')
-    console.log('tring setting')
-    console.log(value)
-    console.log(key)
-    return storageSetPromise(key, value)
+export const getChromeStorage =  (key: string) => {       
+    return window.localStorage.getItem(key) 
 }
 
 
-const storageSetPromise = (key: string, value: string) => {
-    return new Promise((resolve, reject) => {
-        chrome.storage.local.set({ key: value }, () => {
-            console.log('SEEEEETTT***********************')
-            resolve(key)
-        })
-    })
-}
-
-
-export const getChromeStorage = async (key: string) => {
-    console.log(process.env.NODE_ENV)
-    if (process.env.NODE_ENV === "development") {
-        console.log('classic storage')
-        return window.localStorage.getItem(key)
-    }
-    console.log('storage extension')
-    console.log(key)
-    const l = await chrome.storage.local.get([key])
-    console.log(l)
-    return (l).key
-}
-
-
-const authHeader = async () => {
-    const settings: SettingsI = JSON.parse(await getChromeStorage(StorageEnum.SETTINGS) ?? "{}")
+const authHeader =  () => {
+    const settings: SettingsI = JSON.parse(getChromeStorage(StorageEnum.SETTINGS) ?? "{}")
     const auth = btoa(`${settings.mail}:${settings.token}`)
     return {
         Authorization: `Basic ${auth}`,
@@ -50,7 +20,7 @@ const authHeader = async () => {
     }
 }
 export const authAxiosInstance = async () => {
-    const h = await authHeader()
+    const h =  authHeader()
     return axios.create({
         headers: { ...h }
     })
