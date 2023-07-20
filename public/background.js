58455398f963d8c2e0d2f4508157c4ccffde0ad0
ruggero95 <ruggero.panzeri95@gmail.com>
2023-07-20 23:45:18 +0200

    changed rules

--- public/background.js
+++ public/background.js
@@ -1,5 +1,25 @@
-import rules from './rules';
-
+const allResourceTypes = 
+    Object.values(chrome.declarativeNetRequest.ResourceType);
+const rules = [
+  {
+    id: 1,
+    priority: 1,
+    action: {
+      type: chrome.declarativeNetRequest.RuleActionType.MODIFY_HEADERS,
+      responseHeaders: [
+        {
+          operation: chrome.declarativeNetRequest.HeaderOperation.SET,
+          header: 'Access-Control-Allow-Origin',
+          value: '*',
+        },
+      ]
+    },
+    condition: {
+      urlFilter: 'https://*.atlassian.net/rest/agile/1.0',
+      resourceTypes: allResourceTypes,
+    }
+  },
+];
 chrome.declarativeNetRequest.updateDynamicRules({
   removeRuleIds: rules.map((rule) => rule.id), // remove existing rules
   addRules: rules
