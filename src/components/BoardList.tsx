61da82329cd0d593b8ad246c0a8293440f59ee48
ruggero95 <ruggero.panzeri95@gmail.com>
2023-07-22 00:38:19 +0200

    more components

--- src/components/BoardList.tsx
+++ src/components/BoardList.tsx
@@ -18,7 +18,7 @@ export const BoardList: React.FC = () => {
         queryFn: () => getBoards(),
     });
     return (
-        <div className={`grid grid-cols-2 ${isLoading ? 'grid-cols-1' : 'grid-cols-2'} gap-x-4`}>
+        <div className={`grid grid-cols-2 ${isLoading ? 'grid-cols-1 text-center' : 'grid-cols-2'} gap-x-4`}>
             {!isLoading && boards?.values && boards?.values.map((b, i) => {
                 return <CardBoard id={`b-list${i}`} key={`b-list${i}`} board={b} inputName="b-list" ></CardBoard>
             })}
--- src/utils.ts
+++ src/utils.ts
@@ -28,8 +28,7 @@ export const authAxiosInstance = async () => {
 
 export const copyContent = async (text:string) => {
     try {
-      await navigator.clipboard.writeText(text);
-      console.log('Content copied to clipboard');
+      await navigator.clipboard.writeText(text);      
     } catch (err) {
       console.error('Failed to copy: ', err);
     }
